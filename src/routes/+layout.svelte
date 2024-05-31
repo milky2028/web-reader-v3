b74b63f871759c5a25ccc17fb8c23f4e42e95a34
Tyler <tylergross28@gmail.com>
2024-05-31 17:45:47 -0400

    routing and login

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	const routes = [
+		{ path: '/', name: 'Upload' },
+		{ path: '/login', name: 'Log In' }
+	];
+</script>
+
+<div>
+	{#each routes as route, i}<a href={route.path}>{route.name}</a>{i !== routes.length - 1
+			? ' | '
+			: ''}
+	{/each}
+</div>
+<main><slot /></main>
