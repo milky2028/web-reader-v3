b2291ac8fc7699b310a5ccf342e5a3b0d00a7edd
Tyler <tylergross28@gmail.com>
2024-05-31 02:58:24 -0400

    book uploader that also optimizes

---
+++ scripts/upload-book.mjs
@@ -0,0 +1,113 @@
+import { readFile } from 'fs/promises';
+import initialize from 'extract-zip-rar';
+import sharp from 'sharp';
+import { initializeApp, cert } from 'firebase-admin/app';
+import serviceAccount from './cert.json' assert { type: 'json' };
+import { getStorage } from 'firebase-admin/storage';
+import { getFirestore } from 'firebase-admin/firestore';
+
+const [, , archivePath] = process.argv;
+const fullPath = new URL(archivePath, import.meta.url);
+
+globalThis.wasmURL = new URL(
+	'../node_modules/extract-zip-rar/dist/extract.wasm',
+	import.meta.url
+).toString();
+
+function isImage(path) {
+	return path.endsWith('.jpg') || path.endsWith('.png');
+}
+
+const [module, file] = await Promise.all([initialize(), readFile(fullPath)]);
+const {
+	_malloc,
+	open_archive,
+	close_archive,
+	get_next_entry,
+	get_entry_name,
+	entry_is_file,
+	skip_extraction,
+	get_buffer,
+	read_entry_data,
+	get_entry_size,
+	free_buffer,
+	END_OF_FILE,
+	ENTRY_ERROR
+} = module;
+
+function* readArchiveEntries({ file, extractData = false }) {
+	const archivePtr = open_archive(file.ptr, file.size);
+
+	for (;;) {
+		const entryPtr = get_next_entry(archivePtr);
+		if (entryPtr === END_OF_FILE || entryPtr === ENTRY_ERROR) {
+			close_archive(archivePtr);
+			yield null;
+			break;
+		}
+
+		const path = get_entry_name(entryPtr).toLowerCase();
+		const isFile = entry_is_file(entryPtr);
+
+		if (isFile && !path.startsWith('__macosx') && isImage(path)) {
+			const fileName = path.split('/').pop() ?? '';
+			if (extractData) {
+				const size = get_entry_size(entryPtr);
+				const entry_data = read_entry_data(archivePtr, entryPtr);
+				const buffer = get_buffer(entry_data, size);
+				free_buffer(entry_data);
+
+				yield { fileName, buffer };
+			} else {
+				yield { fileName };
+			}
+		} else if (extractData) {
+			skip_extraction(archivePtr);
+		}
+	}
+}
+
+const app = initializeApp({
+	credential: cert(serviceAccount),
+	storageBucket: 'gs://web-reader-ae90f.appspot.com'
+});
+
+const bucket = getStorage(app).bucket();
+const db = getFirestore(app);
+const books = db.collection('books');
+const pages = db.collection('pages');
+
+const bytes = new Uint8Array(file);
+const ptr = _malloc(file.length);
+module.HEAPU8.set(bytes, ptr);
+
+const bookName = decodeURIComponent(
+	fullPath
+		.toString()
+		.split('/')
+		.pop()
+		.replace(/.cbr|.cbz|.zip|.rar/g, '')
+);
+
+console.log('Creating book...');
+const entries = [...readArchiveEntries({ file: { ptr, size: file.length } })];
+
+await books
+	.doc(bookName)
+	.set({ name: bookName, length: entries.length, cover: entries[0].fileName });
+
+for (const entry of readArchiveEntries({ file: { ptr, size: file.length }, extractData: true })) {
+	if (entry) {
+		const path = `${bookName}/${entry.fileName}`;
+		console.log(`Uploading to ${path}...`);
+
+		const optimizedBuffer = await sharp(entry.buffer)
+			.jpeg({ mozjpeg: true, quality: 75 })
+			.toBuffer();
+
+		await Promise.all([
+			bucket.file(path).save(optimizedBuffer),
+			pages.doc(`${bookName}+${entry.fileName}`).set({ name: entry.fileName, book: bookName })
+		]);
+	}
+}
